1f43252e4f545bb7fc08ba5e9e210c694577bed4
Chmielewski Bartosz <T0323061@gemalto.com>
2025-12-30 10:20:50 +0100

    minor changes

--- labopenai.py
+++ labopenai.py
@@ -5,10 +5,8 @@
 ######################################################
 #
 
-
 import os
-from openai import OpenAI
-
+from openai import OpenAI, APITimeoutError
 
 while True:
     data = input("Do you want to talk Directly (1) or via Imperva AI Firewall (2)? ")
@@ -23,33 +21,29 @@ query=""
 
 if direct:
     client = OpenAI()
-    while True:
-        query = input("\nWhat do you want to send to AI? (or type exit)\n  # ")
-        if query == "exit": break
-        response = client.responses.create(
-        model="gpt-5-nano",
-        input=query
-        )
-        print("\n========= The Response: =============\n", response.output_text)
-        print("===================================== ")
-
-
 else:
     client = OpenAI(base_url="https://ai-firewall.aifw-gw.service.imperva.com", 
                     default_headers={"x-imperva-api-key": os.getenv("AIFIREWALL_API_KEY"),
                                     "x-target-url": os.getenv("ORIGINAL_LLM_PROVIDER_URL")})
     
-    while True:
-        query = input("\nWhat do you want to send to AI? (or type exit)\n  # ")
-        if query == "exit": break            
-        response = client.chat.completions.create(
+while True:
+    query = input("\nWhat do you want to send to AI? (or type exit)\n  # ")
+    if query == "exit": break  
+    try:          
+        if direct:
+            response = client.responses.create( model="gpt-5-nano", input=query )
+            responsetext = response.output_text
+
+        else:
+            response = client.chat.completions.create(
             model="gpt-5-nano",
             messages=[{"role": "user", "content": query}],
-        )
-        print("\n========= The Response: =============\n", response.choices[0].message.content)
+            )
+            responsetext = response.choices[0].message.content
+            
+        print("\n========= The Response: =============\n", responsetext)
         print("===================================== ")
-
-
-
-
+        
+    except APITimeoutError:
+        print("\nERROR!!! OpenAI request timed out. Please try again.")
 
